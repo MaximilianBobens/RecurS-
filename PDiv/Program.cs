d759f5b11dc4ffbb9eb50d9aa13b32304c2ecf01
Maximilian Bobens <m.bobens@htlkrems.at>
2024-04-09 13:50:11 +0200

    icha2

--- PDiv/Program.cs
+++ PDiv/Program.cs
@@ -5,5 +5,9 @@ class  Program
     public static void Main(string[] agrs)
     {
         Console.WriteLine("PDiv");
+        for (int i = 0; i < agrs.Length; i++)
+        {
+            Console.WriteLine(agrs[i]);
+        }
     }
 }
